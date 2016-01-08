fbd12a1acd7bb172ac7a020779b98b39d9488ce7
RGLU Capstone <rglucapstone@gmail.com>
2016-01-08 11:45:53 -0500

    Implementacion del componente buscar primera parte
    Tambien se ajusto el listado de preguntas

--- app/src/main/java/com/rglucapstone/activefatherhood/HomeActivity.java
+++ app/src/main/java/com/rglucapstone/activefatherhood/HomeActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -42,6 +43,10 @@ public class HomeActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_activity);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
 
         ArrayList<Question> arrayOfQuestions = new ArrayList<Question>();
         this.populateQuestions(arrayOfQuestions);
@@ -129,6 +134,22 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_buscar:
+                // User chose the "Settings" item, show the app settings UI...
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
     public void profileOn(View view) {
         Intent intent = new Intent(this, ProfileActivity.class);
         startActivity(intent);
--- app/src/main/java/com/rglucapstone/activefatherhood/MainActivity.java
+++ app/src/main/java/com/rglucapstone/activefatherhood/MainActivity.java
@@ -2,9 +2,15 @@ package com.rglucapstone.activefatherhood;
 
 //import android.support.v7.app.AppCompatActivity;
 import android.app.Activity;
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.content.Intent;
+import android.widget.SearchView;
 
 public class MainActivity extends Activity {
 
@@ -13,6 +19,35 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+/*
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.action_bar, menu);
+        return true;
+    }*/
+
+    /*public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the options menu from XML
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.action_bar, menu);
+
+        // Get the SearchView and set the searchable configuration
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.buscar).getActionView();
+        // Assumes current activity is the searchable activity
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
+
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        if (id == R.id.buscar) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }*/
 
     /*public void keepOn(View view) {
         Intent intent = new Intent(this, CategoriesActivity.class);
@@ -32,4 +67,5 @@ public class MainActivity extends Activity {
         Intent intent = new Intent(this, RegisterActivity.class);
         startActivity(intent);
     }
+
 }
